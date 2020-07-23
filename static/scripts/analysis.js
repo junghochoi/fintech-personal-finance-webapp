9541cdeb01d21eb4a94ef15f2c867d371b4a20f0
junghole <dchoi315@gmail.com>
2020-07-23 12:16:13 -0400

    update Chart button

--- static/scripts/analysis.js
+++ static/scripts/analysis.js
@@ -1,19 +1,33 @@
+
+
+console.log("connected")
+
+userData =  {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+};
 let ctx = document.getElementById('income-chart').getContext('2d');
 let chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
 
     // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+    data: userData,
 
     // Configuration options go here
     options: {}
+});
+
+
+$(document).ready(()=>{
+    $('#update').on('click', ()=>{
+        userData.datasets[0].data[0]= 100;
+        chart.update()
+    });
+
 });
