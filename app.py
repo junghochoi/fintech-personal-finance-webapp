0aae0286a6844cc955b9deb1ef4da4fdf8a5ce6c
Alice Yeh <aliceyeh12@gmail.com>
2020-07-23 16:27:48 +0000

    Update homepage

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect, url_for
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+load_dotenv()
+
+app.config['MONGO_DBNAME'] = 'ftf-final'
+app.config['MONGO_URI'] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", title="Login")
+
+@app.route('/home')
+def home():
+    collection = mongo.db.posts
+    posts = collection.find({}).sort('date')
+    return render_template("homepage.html", posts=posts)
+
+@app.route('/home/add-post', methods=["GET", "POST"])
+def add_post():
+    if request.method == "GET":
+        retu
+        return redirect(url_for("homepage.html"))
+    else:
+        posts = mongo.db.posts
+        posts.insert({"post": request.form["post-message"], "category": request.form["post-category"]})
+        # return redirect(url_for("homepage.html"))
+        return render_template("template.html", music=music)
 
 @app.route('/analysis')
 def analysis():
